fa2abf261da1d714d98c904ed480926c1e977552
freelancerjunayed junayed <freelancerjunayedjunayed@gmail.com>
2026-06-28 11:42:42 +0600

    pudated

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -2,27 +2,22 @@ import { createBrowserRouter } from "react-router";
 import Home from "../Components/Home";
 import Layout from "../layout/Layout";
 import About from "../Components/About";
-import Booking from "../Components/BookingForm";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        component: <Layout />,
-        children: [
-            {
-                index: true,
-                component: <Home />
-            },
-            {
-                path: "/about",
-                component: <About></About>
-            },
-            {
-                path: "/booking",
-                component: <BookingForm></BookingForm>
-            }
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About></About>
+      }
+    ],
+  },
+]);
 
 export default router;
